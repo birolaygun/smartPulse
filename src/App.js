5957cb91fc565231359ce011c11bd7884917574a
birol  aygun <birolaygun.bilisim@gmail.com>
2022-04-03 03:45:52 +0300

    firs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [rates, setRates] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://seffaflik.epias.com.tr/transparency/service/market/intra-day-trade-history?endDate=2022-01-26&startDate=2022-01-26"
+      )
+      .then((data) => setRates(data))
+      .catch((err) => console.log("err", err));
+  }, []);
+
+  console.log(rates);
+
+  return <div className="App">gd</div>;
 }
 
 export default App;
